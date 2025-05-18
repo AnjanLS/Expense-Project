65709ac4dc6f6145a4f9238e551e6dac34f89e8d
AnjanLS <anjansriram.98@gmail.com>
2025-05-19 00:26:42 +0530

    latest-changes

--- 04-Deleting-old-logs.sh
+++ 04-Deleting-old-logs.sh
@@ -5,52 +5,64 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR=$1
-DESTINATION_DIR=$2
-DAYS=${3:-15}       #Assigning the default value to the third positional argument i.e, number of days
-
-LOGS_FOLDER="/home/ec2-user/Expense-Project/expense-logs"
+SOURCE_DIR="/home/ec2-user/Expense-Project/expense-logs"
+DESTINATION_DIR="s3://app1-expense-project-anjansriram.com/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$SOURCE_DIR/$LOG_FILE-$TIMESTAMP.log"
+TMP_DIR="/tmp"
 
 echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-USAGE(){
-    echo -e "$R USAGE:: $N sh 04-Deleting-old-logs.sh <SOURCE_DIR> <DESTINATION_DIR> <DAYS(optional)>"
-}
-
-if [ $# -lt 2 ]; then
-    USAGE
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$SOURCE_DIR does not exist... please check it."
     exit 1
-    if [ ! -d $SOURCE_DIR ]; then
-        echo -e "$SOURCE_DIR does not exist... please check it."
+    if [ ! -d $DESTINATION_DIR ]; then
+        echo -e "$DESTINATION_DIR does not exist... please check it."
         exit 1
-        if [ ! -d $DESTINATION_DIR ]; then
-            echo -e "$DESTINATION_DIR does not exist... please check it."
-            exit 1
-        fi
     fi
 fi
 
+# Create zip archive of log files
+echo -e "Zipping log files..."
+zip -j "$TMP_DIR/$LOG_FILE_NAME" "$SOURCE_DIR"/*.log &>/dev/null
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+if [ $? -ne 0 ]; then
+  echo -e "$R Failed to create zip file. Aborting.$N"
+  exit 1
+fi
 
-if [ -n $FILES ]; then
-    echo "FILES are: $FILES"
-    ZIP_FILE="$DESTINATION_DIR-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip @ "$ZIP_FILE"
-    if [ -f "$ZIP_FILE" ]; then
-        echo -e "Successfully created zip file for the files older than $DAYS"
-        while read -r file
-        do
-            echo "Deleting File: $file"
-            rm -rf $file
-        done <<< $FILES
-    else
-        echo -e "$R ERROR:: $N Failed to create zip file"
-        exit 1
-    fi
+# Upload to S3
+echo -e "Uploading $LOG_FILE_NAME to S3..."
+aws s3 cp "$TMP_DIR/$LOG_FILE_NAME" "$DESTINATION_DIR"
+
+if [ $? -eq 0 ]; then
+  echo -e "$G Upload successful. Deleting original logs...$N"
+  rm -f "$SOURCE_DIR"/*.log
+  echo -e "$G Log files deleted from server.$N"
 else
-    echo "No files found older than $DAYS"
+  echo -e "$R Upload failed. Logs not deleted.$N"
 fi
+
+
+
+# FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+# if [ -n $FILES ]; then
+#     echo "FILES are: $FILES"
+#     ZIP_FILE="$DESTINATION_DIR-$TIMESTAMP.zip"
+#     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip @ "$ZIP_FILE"
+#     if [ -f "$ZIP_FILE" ]; then
+#         echo -e "Successfully created zip file for the files older than $DAYS"
+#         while read -r file
+#         do
+#             echo "Deleting File: $file"
+#             rm -rf $file
+#         done <<< $FILES
+#     else
+#         echo -e "$R ERROR:: $N Failed to create zip file"
+#         exit 1
+#     fi
+# else
+#     echo "No files found older than $DAYS"
+# fi
