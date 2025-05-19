7824e41f543cb22aa9d56c8669735f9e299d8c4e
AnjanLS <anjansriram.98@gmail.com>
2025-05-19 19:51:20 +0530

    latest-changes

--- 04-Deleting-old-logs.sh
+++ 04-Deleting-old-logs.sh
@@ -9,10 +9,11 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-7} # if user is not providing number of days, we are taking week as default
 
-LOGS_FOLDER="/home/ec2-user/Expense-Project/expense-logs"
+# LOGS_FOLDER="/home/ec2-user/Expense-Project/expense-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOG_FILE-$TIMESTAMP.log"
+# LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
